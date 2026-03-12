afbbf27fab96b9075f38eea0f6b8f2e9c3d1fa4e
llelant <llelant@iut.univ-paris8.fr>
2026-03-12 10:00:49 +0100

    code classee enum

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr15/commons/entites/enums/LangueEnum.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr15/commons/entites/enums/LangueEnum.java
@@ -1,5 +1,26 @@
 package universite_Paris8.iut.qdev.tp2026.gr15.commons.entites.enums;
 
 public enum LangueEnum {
-    francais, english, deutch, espana, italia
+    FRANCAIS("Français", 1),
+    ENGLISH("English", 2),
+    DEUTSCH("Deutsch", 3),
+    ESPANOL("Español", 4),
+    ITALIANO("Italiano", 5);
+
+    private final String nom;
+    private final int id;
+
+    LangueEnum(String nom, int id) {
+        this.nom = nom;
+        this.id = id;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
 }
