8377b6a8711f9b6175ae14d8f8a8e81c51d76183
ktshibola <kurtystshibola@gmail.com>
2026-03-26 13:50:04 +0100

    test & mock base

--- src/test/java/universite_Paris8/iut/qdev/tp2026/gr15/AppTest.java
+++ src/test/java/universite_Paris8/iut/qdev/tp2026/gr15/AppTest.java
@@ -1,17 +1,34 @@
 package universite_Paris8.iut.qdev.tp2026.gr15;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import mocks.AjouterJoueurKOprenomnull;
 import org.junit.jupiter.api.Test;
+import universite_Paris8.iut.qdev.tp2026.gr15.interfaces.IservicesJoueur;
+import universite_Paris8.iut.qdev.tp2026.gr15.utils.exceptions.PrenomInvalideException;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
+
     /**
      * Rigorous Test :-)
      */
+
+    /**
+     * Prénom null → PrenomInvalideException attendue.
+     */
+    @Test
+    public void creerJoueur_prenomNull_levePrenomInvalideException() {
+        IservicesJoueur serviceJoueur = new AjouterJoueurKOprenomnull();
+        assertThrows(PrenomInvalideException.class, () ->
+                serviceJoueur.creerJoueur(null, "alice99", 2000, 1, "sport")
+        );
+    }
+
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
